82711020a13893f99a0b2075b5cbf2a83f8361b6
Rut Patel <60327079+rutpatel121@users.noreply.github.com>
2020-03-31 20:55:24 -0400

    Update Project2-Part1.py

--- Project2-Part1.py
+++ Project2-Part1.py
@@ -1,5 +1,4 @@
 import random
-import time
 
 class Graph():
 
@@ -30,86 +29,142 @@ class Graph():
         #"""This returns a set of all Nodes in the graph"""
     def getAllNodes(self):
         return list(self.graph_dict.keys())
-
-
+graph = Graph()
 class GraphSearch():
-
-    def dfsrec(self,graph, start, goal, path=None):
-        if path is None:
-            path = [start]
+    def dfs(self, start, goal, path, traversed):
+        path.append(start)
+        traversed.add(start)
         if start == goal:
-            yield path
-        for next in graph[start] - set(path):
-            yield from DFSRec(graph, next, goal, path + [next])
+            return 1
+        for node in graph.graph_dict[start]:
+            if node not in traversed:
+                result = self.dfs(node, goal, path, traversed)
+                if result is not None:
+                    return path
+        return None
+
+    def dfsrec(self, start, goal, path=None):
+        path = []
+        traversed = set()
+        return self.dfs( start, goal, path,traversed )
 
 
     #"""Iteratively returns an ArrayList of the Nodes in the Graph in a valid Depth-First Search order."""
-    def dfsiter(self,graph, start, goal):
-        stack = [(start, [start])]
+    def dfsiter(self, start, goal,path=None):
+        path = []
+        traversed= set()
+        stack = [start]
         while stack:
-            (vertex, path) = stack.pop()
-            for next in graph[vertex] - set(path):
-                if next == goal:
-                    yield path + [next]
-                else:
-                    stack.append((next, path + [next]))
+            forward = stack.pop()
+            path.append(forward)
+            if forward == goal:
+                return path
+            traversed.add(forward)
+            connection = graph.graph_dict[forward]
+            for node in reversed(connection):
+                if node not in traversed:
+                    stack.append(node)
 
 
 
     # """Recursively returns an List of the Nodes in the Graph in a valid Breadth-First Traversal order."""
-    def bbftrec(self,node):
-        output_list.append(node)
-    def bftrec(self,graph,start,end):
-        i=1
-        while i<=int(end):
-            self.bbftrec(str(i))
-            i=i+1
 
+    def bft(self,graph, queue, path, traversed):
+        if len(queue) != 0:
+            forward = queue.pop(0)
+            path.append(forward)
+            for connection in graph.graph_dict[forward]:
+                if connection not in traversed:
+                    queue.append(connection)
+                    traversed.add(forward)
+            return self.bft(graph, queue, path, traversed)
+    def bftrec(self,graph):
+        path = []
+        traversed = set()
+        for vertix in graph.graph_dict:
+            if vertix not in traversed:
+                self.bft(graph, [vertix], path, traversed)
+        return path
     #""" Iteratively returns an List of all of the Nodes in the Graph in a valid Breadth-First Traversal."""
-    def bftiter(self,graph, start):
-        visited, queue = set(), [start]
-        while queue:
-            vertex = queue.pop(0)
-            if vertex not in visited:
-                visited.add(vertex)
-                queue.extend(graph[vertex] - visited)
-        return visited
+    def bfti(self,graph, queue, path, traversed):
+        while len(queue) != 0:
+            forward = queue.pop(0)
+            path.append(forward)
+            for vertix in graph.graph_dict[forward]:
+                if vertix not in traversed:
+                    traversed.add(vertix)
+                    queue.append(vertix)
+    def bftiter(self,graph):
+        path= []
+        traversed = set()
+        for vertix in graph.graph_dict:
+            if vertix not in traversed:
+                self.bfti(graph, [vertix], path, traversed)
+        return path
 
 
 if __name__ == "__main__":
-    output_list=[]
-    graph = Graph()
     linked_list = Graph()
-    search=GraphSearch()
+    search = GraphSearch()
     #"""Creates n random nodes with randomly assigned unweighted, bidirectional edges."""
     def createRandomUnweightedGraphIter(n):
 
         i=1
         while i<=n:
             k=random.randrange(1, 5, 1)
-            graph.addNode(str(i))
+            graph.addNode(i)
             if i>=5:
                 l=0
                 while l<k:
                     rand_node=random.randrange(1, i, 1)
-                    graph.addUndirectedEdge(str(rand_node),str(i))
+                    graph.addUndirectedEdge(rand_node,i)
                     l=l+1
             i=i+1
     #"""Creates a Graph with n nodes where each node only has an edge to the next node created."""
     def createLinkedList(n):
         if n>0:
-            linked_list.addNode("1")
+            linked_list.addNode(1)
             i=2
             while i<=n:
-                linked_list.addNode(str(i))
-                linked_list.addUndirectedEdge(str(i-1),str(i))
+                linked_list.addNode(i)
+                linked_list.addUndirectedEdge(i-1,i)
                 i=i+1
-    createLinkedList(1000)
     #"""Run a BFT recursively on a LinkedList"""
     def BFTRecLinkedList(link_list):
-        search.bftrec(link_list.keys(),"1","10000")
+        return search.bftrec(link_list)
+
     #run a BFT iteratively on a LinkedList
-    BFTRecLinkedList(linked_list.graph_dict)
-    print(output_list)
+
     def BFTIterLinkedList(link_list):
-        return search.bftiter(linked_list,"1")
+        return search.bftiter(link_list)
+
+
+    createRandomUnweightedGraphIter(10)
+    createLinkedList(10)
+
+    print("*****************Graphs*****************")
+    print ()
+    print ("graph adjacency list------>",graph.graph_dict)
+    print ()
+    print("DFSRec------>",search.dfsrec(2,8))
+    print ()
+    print("DFSIter------>",search.dfsiter(2,8))
+    print ()
+    print ("BFTRec------>",search.bftrec(graph))
+    print ()
+    print ("BFTIter------>",search.bftiter(graph))
+    print ()
+    graph=linked_list
+    print("*****************Linked List*****************")
+    print ()
+    print("DFSRec------>",search.dfsrec(2,8))
+    print ()
+    print("DFSIter------>",search.dfsiter(2,8))
+    print ()
+    print ("BFTRec------>",search.bftrec(linked_list))
+    print ()
+    print ("BFTIter------>",search.bftiter(linked_list))
+    print ()
+    print ("BFTRecLinkedList----->",BFTRecLinkedList(linked_list))
+    print ()
+    print ("BFTIterLinkedList----->",BFTIterLinkedList(linked_list))
